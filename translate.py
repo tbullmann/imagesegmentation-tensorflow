5218a996f689ff3de78e9c1beff9961734428b98
tbullmann <bullmann.torsten@gmail.com>
2019-04-04 20:33:38 +0200

    comment on dynamic memory allocation

--- translate.py
+++ translate.py
@@ -13,6 +13,14 @@ import collections
 import math
 import time
 
+# # dynamic memory allocation
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+# config.log_device_placement = True  # to log device placement (on which device the operation ran)
+#                                     # (nothing gets printed in Jupyter, only if you run it standalone)
+# sess = tf.Session(config=config)
+# set_session(sess)  # set this TensorFlow session as the default session for Keras
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", required=True, choices=["train", "test", "predict"])
