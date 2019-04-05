145212a362f91a42fe0854c9bc5a8453d8beb684
tbullmann <bullmann.torsten@gmail.com>
2019-04-05 07:04:29 +0200

    longer training

--- docs/2D_3Labels/survey_networks_depth.sh
+++ docs/2D_3Labels/survey_networks_depth.sh
@@ -7,8 +7,8 @@
 #    --target_dir  datasets/snemi3d/label3/ \
 #    --output_dir datasets/snemi3d/input2label3/
 
-python tools/split.py \
-    --dir datasets/snemi3d/input2label3/
+#python tools/split.py \
+#    --dir datasets/snemi3d/input2label3/
 
 ### Train
 
@@ -19,10 +19,10 @@ do
 	((counter++))
 	python translate.py    --mode train \
 	--network unet  --u_depth $n_depth  \
-	--output_dir temp/publication2/how_deep/train/$counter \
+	--output_dir temp/publication3/how_deep/train/$counter \
 	--input_dir datasets/snemi3d/input2label3/train \
-	  --loss square  \
-	--display_freq 500  --max_epochs 200
+	--loss square  --batch_size 8  \
+	--display_freq 2000  --max_epochs 2000
 done
 
 for n_dense_blocks in 1 2 3 4 5;
@@ -30,10 +30,10 @@ do
 	((counter++))
 	python translate.py    --mode train \
 	--network densenet  --n_dense_blocks $n_dense_blocks  \
-	--output_dir temp/publication2/how_deep/train/$counter \
+	--output_dir temp/publication3/how_deep/train/$counter \
 	--input_dir datasets/snemi3d/input2label3/train \
-	--loss square  \
-	--display_freq 500  --max_epochs 200
+	--loss square  --batch_size 8  \
+	--display_freq 2000  --max_epochs 2000
 done
 
 for n_highway_units in 4 6 9 12 16;
@@ -41,10 +41,10 @@ do
 	((counter++))
 	python translate.py    --mode train \
 	--network highwaynet  --n_highway_units $n_highway_units  \
-	--output_dir temp/publication2/how_deep/train/$counter \
+	--output_dir temp/publication3/how_deep/train/$counter \
 	--input_dir datasets/snemi3d/input2label3/train \
-	--loss square  \
-	--display_freq 500  --max_epochs 200
+	--loss square  --batch_size 8  \
+	--display_freq 2000  --max_epochs 2000
 done
 
 for n_res_blocks in 4 6 9 12 16;
@@ -52,10 +52,10 @@ do
 	((counter++))
 	python translate.py    --mode train \
 	--network resnet  --n_res_blocks $n_res_blocks \
-	--output_dir temp/publication2/how_deep/train/$counter \
+	--output_dir temp/publication3/how_deep/train/$counter \
 	--input_dir datasets/snemi3d/input2label3/train \
-	--loss square  \
-	--display_freq 500  --max_epochs 200
+	--loss square  --batch_size 8  \
+	--display_freq 2000  --max_epochs 2000
 done
 
 
@@ -64,8 +64,8 @@ done
 for i in `seq 1 $counter`;
 do
 	python translate.py   --mode test \
-	--checkpoint temp/publication2/how_deep/train/$i \
-	--output_dir temp/publication2/how_deep/test/$i \
+	--checkpoint temp/publication3/how_deep/train/$i \
+	--output_dir temp/publication3/how_deep/test/$i \
 	--input_dir datasets/snemi3d/input2label3/val \
     --image_width 1024  --image_height 1024
 done
@@ -74,9 +74,9 @@ done
 #
 #for i in `seq 1 $counter`;
 #do
-#	bash tools/evaluate.sh temp/publication2/how_deep/test/$i synapses
-#	bash tools/evaluate.sh temp/publication2/how_deep/test/$i mitochondria
-#	bash tools/evaluate.sh temp/publication2/how_deep/test/$i membranes
+#	bash tools/evaluate.sh temp/publication3/how_deep/test/$i synapses
+#	bash tools/evaluate.sh temp/publication3/how_deep/test/$i mitochondria
+#	bash tools/evaluate.sh temp/publication3/how_deep/test/$i membranes
 #done
 #
 #
@@ -87,14 +87,14 @@ done
 #
 
 # NOTE: Information about generator type / depath can be obtained by order of files only
-#sed -n 1p temp/publication2/how_deep/test/1/evaluation/membranes.csv > temp/publication2/how_deep/evaluation-membranes.csv
-#sed -n 1p temp/publication2/how_deep/test/1/evaluation/mitochondria.csv > temp/publication2/how_deep/evaluation-mitochondria.csv
-#sed -n 1p temp/publication2/how_deep/test/1/evaluation/synapses.csv > temp/publication2/how_deep/evaluation-synapses.csv
+#sed -n 1p temp/publication3/how_deep/test/1/evaluation/membranes.csv > temp/publication3/how_deep/evaluation-membranes.csv
+#sed -n 1p temp/publication3/how_deep/test/1/evaluation/mitochondria.csv > temp/publication3/how_deep/evaluation-mitochondria.csv
+#sed -n 1p temp/publication3/how_deep/test/1/evaluation/synapses.csv > temp/publication3/how_deep/evaluation-synapses.csv
 #for i in `seq 1 $counter`;
 #do
-#	sed 1d temp/publication2/how_deep/test/$i/evaluation/membranes.csv >> temp/publication2/how_deep/evaluation-membranes.csv
-#	sed 1d temp/publication2/how_deep/test/$i/evaluation/mitochondria.csv >> temp/publication2/how_deep/evaluation-mitochondria.csv
-#	sed 1d temp/publication2/how_deep/test/$i/evaluation/synapses.csv >> temp/publication2/how_deep/evaluation-synapses.csv
+#	sed 1d temp/publication3/how_deep/test/$i/evaluation/membranes.csv >> temp/publication3/how_deep/evaluation-membranes.csv
+#	sed 1d temp/publication3/how_deep/test/$i/evaluation/mitochondria.csv >> temp/publication3/how_deep/evaluation-mitochondria.csv
+#	sed 1d temp/publication3/how_deep/test/$i/evaluation/synapses.csv >> temp/publication3/how_deep/evaluation-synapses.csv
 #done
 
 
