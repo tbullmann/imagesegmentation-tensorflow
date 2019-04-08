4287970b7e114fcf2435331e81b336a1d7bc41e7
tbullmann <bullmann.torsten@gmail.com>
2019-04-08 17:54:07 +0200

    standard model for SNEMI3D and VNC datasets

---
+++ docs/2D_3Labels/standard_SNEMI3D.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+### Prepare data
+python tools/process.py \
+    --operation combine \
+    --input_dir  datasets/snemi3d/input/ \
+    --target_dir  datasets/snemi3d/label3/ \
+    --output_dir datasets/snemi3d/input2label3/
+
+python tools/split.py \
+    --dir datasets/snemi3d/input2label3/
+
+### Train
+
+
+	python translate.py    --mode train \
+	--network resnet \
+	--output_dir temp/temp/standard_SNEMI3D/train \
+	--input_dir datasets/snemi3d/input2label3/train \
+	--loss square  --batch_size 8  \
+	--display_freq 2000  --max_epochs 2000
+
+
+### Test
+
+	python translate.py   --mode test \
+	--checkpoint temp/temp/standard_SNEMI3D/train \
+	--output_dir temp/temp/standard_SNEMI3D/test \
+	--input_dir datasets/snemi3d/input2label3/val \
+    --image_width 1024  --image_height 1024
+
+#### Evaluate
+# bash tools/evaluate.sh temp/standard_SNEMI3D/test synapses
+# bash tools/evaluate.sh temp/standard_SNEMI3D/test mitochondria
+# bash tools/evaluate.sh temp/standard_SNEMI3D/test membranes
+
