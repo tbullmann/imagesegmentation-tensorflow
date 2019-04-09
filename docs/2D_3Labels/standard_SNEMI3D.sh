2c85e166d68ed48df7da8f6eb98a29168c7222c6
tbullmann <bullmann.torsten@gmail.com>
2019-04-09 09:20:48 +0200

    train, test, evaluate default networks for VCN and SNEMI3D

--- docs/2D_3Labels/standard_SNEMI3D.sh
+++ docs/2D_3Labels/standard_SNEMI3D.sh
@@ -3,34 +3,30 @@
 ### Prepare data
 python tools/process.py \
     --operation combine \
-    --input_dir  datasets/snemi3d/input/ \
+    --input_dir  datasets/snemi3d/clahe/ \
     --target_dir  datasets/snemi3d/label3/ \
-    --output_dir datasets/snemi3d/input2label3/
+    --output_dir datasets/snemi3d/clahe2label3/
 
 python tools/split.py \
-    --dir datasets/snemi3d/input2label3/
+    --dir datasets/snemi3d/clahe2label3/
 
 ### Train
-
-
-	python translate.py    --mode train \
+python translate.py    --mode train \
 	--network resnet \
-	--output_dir temp/temp/standard_SNEMI3D/train \
-	--input_dir datasets/snemi3d/input2label3/train \
+	--output_dir temp/default/SNEMI3D/train \
+	--input_dir datasets/snemi3d/clahe2label3/train \
 	--loss square  --batch_size 8  \
 	--display_freq 2000  --max_epochs 2000
 
-
 ### Test
-
-	python translate.py   --mode test \
-	--checkpoint temp/temp/standard_SNEMI3D/train \
-	--output_dir temp/temp/standard_SNEMI3D/test \
-	--input_dir datasets/snemi3d/input2label3/val \
+python translate.py   --mode test \
+	--checkpoint temp/default/SNEMI3D/train \
+	--output_dir temp/default/SNEMI3D/test \
+	--input_dir datasets/snemi3d/clahe2label3/val \
     --image_width 1024  --image_height 1024
 
-#### Evaluate
-# bash tools/evaluate.sh temp/standard_SNEMI3D/test synapses
-# bash tools/evaluate.sh temp/standard_SNEMI3D/test mitochondria
-# bash tools/evaluate.sh temp/standard_SNEMI3D/test membranes
+### Evaluate
+bash tools/evaluate.sh temp/default/SNEMI3D/test synapses
+bash tools/evaluate.sh temp/default/SNEMI3D/test mitochondria
+bash tools/evaluate.sh temp/default/SNEMI3D/test membranes
 
--- docs/2D_3Labels/standard_VCN.sh
+++ docs/2D_3Labels/standard_VCN.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bash
 
-#### Prepare data
-#python tools/process.py \
-#    --operation combine \
-#    --input_dir  datasets/vnc/raw/ \
-#    --target_dir  datasets/vnc/labels/ \
-#    --output_dir datasets/vnc/combined/
+### Prepare data
+python tools/process.py \
+    --operation combine \
+    --input_dir  datasets/vnc/raw/ \
+    --target_dir  datasets/vnc/labels/ \
+    --output_dir datasets/vnc/combined/
 
-#python tools/split.py \
-#    --dir datasets/vnc/combined/
+python tools/split.py \
+    --dir datasets/vnc/combined/
 
 ### Train
-
-
-	python translate.py    --mode train \
+python translate.py    --mode train \
 	--network resnet \
-	--output_dir temp/temp/standard_VCN/train \
+	--output_dir temp/default/VCN/train \
 	--input_dir datasets/vnc/combined/train \
 	--loss square  --batch_size 8  \
 	--display_freq 2000  --max_epochs 2000
 
-
 ### Test
-
-	python translate.py   --mode test \
-	--checkpoint temp/temp/standard_VCN/train \
-	--output_dir temp/temp/standard_VCN/test \
+python translate.py   --mode test \
+	--checkpoint temp/default/VCN/train \
+	--output_dir temp/default/VCN/test \
 	--input_dir datasets/vnc/combined/val \
     --image_width 1024  --image_height 1024
 
-#### Evaluate
-# bash tools/evaluate.sh temp/standard_VCN/test synapses
-# bash tools/evaluate.sh temp/standard_VCN/test mitochondria
-# bash tools/evaluate.sh temp/standard_VCN/test membranes
+### Evaluate
+bash tools/evaluate.sh temp/default/VCN/test synapses
+bash tools/evaluate.sh temp/default/VCN/test mitochondria
+bash tools/evaluate.sh temp/default/VCN/test membranes
 
