d126d50f8756447b9e3e1c2b5db16d2cf1b35690
tbullmann <bullmann.torsten@gmail.com>
2019-10-24 08:41:06 +0200

    semantic vs instance segmentation scores description

--- docs/2D_3Labels/score_less_annotation_SNEMI3D.sh
+++ docs/2D_3Labels/score_less_annotation_SNEMI3D.sh
@@ -3,7 +3,7 @@
 ### Evaluate
 for i in `seq 1 4`;
 do
-	bash tools/evaluate.sh temp/survey_less_annotation/snemi3d/test/$i synapses
-	bash tools/evaluate.sh temp/survey_less_annotation/snemi3d/test/$i mitochondria
-	bash tools/evaluate.sh temp/survey_less_annotation/snemi3d/test/$i membranes
+	bash tools/evaluate.sh temp/survey_less_annotation/test/$i synapses
+	bash tools/evaluate.sh temp/survey_less_annotation/test/$i mitochondria
+	bash tools/evaluate.sh temp/survey_less_annotation/test/$i membranes
 done
